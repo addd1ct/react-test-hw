64c594644e62f373386da414a408156f7f8f233f
polly <polifibliss@gmail.com>
2025-06-22 15:34:12 +0300

    edit

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 export function errorHandler(err, req, res, next) {
+  console.error(err);
   const { status = 500, message = 'Internal Server Error' } = err;
   res.status(status).json({
     status,
